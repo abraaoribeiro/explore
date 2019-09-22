ac36746e867e54d91dab5809599bbd8f85026595
abraaoribeiro <abraaoribeiro@localhost.localdomain>
2019-09-22 15:21:28 -0300

    item colapsse tela de detalhe [incompleto]

--- src/app/pages/place/place-detail/place-detail.page.ts
+++ src/app/pages/place/place-detail/place-detail.page.ts
@@ -1,3 +1,4 @@
+import { LoadingController } from '@ionic/angular';
 import { GoogleMapsService } from 'src/app/service/google-maps.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,8 +11,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PlaceDetailPage implements OnInit {
 
   place: any;
+  shownGroup = null;
 
-  constructor(private googleMapsService: GoogleMapsService, 
+  constructor(private googleMapsService: GoogleMapsService,
+    public loadingController: LoadingController,
     private route: ActivatedRoute,
     private router: Router) { }
 
@@ -19,15 +22,34 @@ export class PlaceDetailPage implements OnInit {
     this.getPlaceDetail()
   }
 
-  getPlaceDetail() {
+  public async getPlaceDetail() {
+    let loading = await this.loadingController.create({
+      spinner: 'dots',
+      mode: 'ios',
+      cssClass: 'spinner',
+      animated: true
+    });
+    await loading.present();
     let place_id = this.route.snapshot.params['id'];
-    this.googleMapsService.getPlaceDetail('', place_id).then(placeDetail => {
+    await this.googleMapsService.getPlaceDetail('', place_id).then(placeDetail => {
       console.log(placeDetail);
       this.place = placeDetail;
+      loading.dismiss();
     })
   }
 
-  buttonBack(){
+  public toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+      this.shownGroup = null;
+    } else {
+      this.shownGroup = group;
+    }
+  };
+  public isGroupShown(group) {
+    return this.shownGroup === group;
+  };
+
+  buttonBack() {
     this.router.navigateByUrl('/tabs/tab1')
   }
 }
